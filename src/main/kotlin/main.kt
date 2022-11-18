11b36dd5ca356609e84626a613fb6969afb5dc79
Richard.Moss <richard.in.oman@gmail.com>
2022-11-18 16:56:06 +0800

    Playing with branches

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,15 +1,9 @@
 fun main () {
 
-    val nutrients: Map<String, Double> = mapOf(
-        "c" to 123.0,
-        "n" to 40.0,
-        "p" to 19.0,
-        "k" to 31.0,
-        "s" to 4.0
-    )
+// Lets make 100 Ingredient objects
 
-    val input = nutrients.keys
-
-    input.forEach { println("${it.uppercase()}") }
+    val ingredients: MutableList<Ingredient> = mutableListOf()
+    for (i in 1..100) { ingredients.add(Ingredient("$i")) }
+    println(ingredients.size)
 
 }
